9d09551b453fe527a52f7d71a63e91bc7e66d2e3
Subrahmanya <sghegde203@gmail.com>
2025-08-04 05:21:45 +0000

    Initial commit - AVM project

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+<div class="row">
+                            <div class="col-md-12">
+                                <div class="copyright">
+                                    <p style="color: #f9ff00; line-height: calc(500/15); font-size: 16px">Apartment Visitors Management.com All rights reserved.</p>
+                                </div>
+                            </div>
+                        </div>
